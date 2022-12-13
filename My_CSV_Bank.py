b0ca1c011ab55b51caed15a074dc02e7e9625bc1
Artur <atrojmiasto@gmail.com>
2022-12-14 00:35:05 +0100

    Aktualizacja

--- My_CSV_Bank.py
+++ My_CSV_Bank.py
@@ -20,16 +20,19 @@ def odczyt():
             print(os.path.join(file))
 
 
-    plik = 'lista_operacji.csv'
-    with open(plik, 'r') as file:
-        data = csv.reader(file, delimiter=';')
+    plik = 'lista_operacji_2.csv'
+    with open(plik, 'r', encoding='utf-8') as file:  # encoding='utf-8'
+        data = csv.DictReader(file, delimiter=';')  # csv.DictReader po nazwie kolumny
+        # data = csv.reader(file, delimiter=';')  # csv.reader po nr kolumny
         lista = []
         for row in data:
             lista.append(row)
         # print(lista[1:])
 
         for element in lista[1:3]:
-            print(element[3], element[4])
+
+            print(element['Kategoria'], element['Kwota operacji'])
+            # print(element[4], element[5])
 
 
 odczyt()
